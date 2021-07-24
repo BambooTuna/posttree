caf1961acda189c67ea3684e737359c94b8384f0
BambooTuna <bambootuna@gmail.com>
2021-07-24 20:26:13 +0900

    Fix: keyboard space

--- lib/ui/post_form.dart
+++ lib/ui/post_form.dart
@@ -56,39 +56,37 @@ class _PostFormState extends State<PostForm> {
   @override
   Widget build(BuildContext context) {
     final viewModel = Provider.of<PostFormViewModel>(context);
-    return Container(
-      height: 400,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text("まだ投稿はできないけど、投稿できる場所だよ"),
-            Text(
-              viewModel.errorMessage ?? "",
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyText1
-                  ?.copyWith(color: Theme.of(context).errorColor),
-            ),
-            TextFormField(
-                autofocus: true,
-                initialValue: _initialValue,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: "何か入力してね",
-                ),
-                onChanged: (text) {
-                  viewModel.setContent(text);
-                }),
-            ElevatedButton(
-              child: Text("Send: " + viewModel.content),
-              onPressed: () {
-                viewModel.send();
-              },
-            )
-          ],
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text("まだ投稿はできないけど、投稿できる場所だよ"),
+          Text(
+            viewModel.errorMessage ?? "",
+            style: Theme.of(context)
+                .textTheme
+                .bodyText1
+                ?.copyWith(color: Theme.of(context).errorColor),
+          ),
+          TextFormField(
+              autofocus: true,
+              initialValue: _initialValue,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "何か入力してね",
+              ),
+              onChanged: (text) {
+                viewModel.setContent(text);
+              }),
+          ElevatedButton(
+            child: Text("Send: " + viewModel.content),
+            onPressed: () {
+              viewModel.send();
+            },
+          ),
+          SizedBox(height: 48),
+        ],
       ),
     );
   }
