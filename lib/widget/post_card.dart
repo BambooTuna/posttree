814b703b447d0d6d856eda527944542ce13db4a8
BambooTuna <bambootuna@gmail.com>
2021-08-01 22:28:59 +0900

    New: flutter link preview

--- lib/widget/post_card.dart
+++ lib/widget/post_card.dart
@@ -1,9 +1,13 @@
+import 'package:any_link_preview/any_link_preview.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_link_preview/flutter_link_preview.dart';
 import 'package:flutter_parsed_text/flutter_parsed_text.dart';
 import 'package:posttree/model/post.dart';
 import 'package:posttree/ui/user_page.dart';
 import 'package:posttree/widget/user_icon.dart';
-import 'package:simple_url_preview/simple_url_preview.dart';
+import 'package:universal_platform/universal_platform.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PostCard extends StatelessWidget {
   final Post item;
@@ -61,19 +65,110 @@ class PostCard extends StatelessWidget {
                             renderWidget: (
                                 {required String pattern,
                                 required String text}) {
-                              return SimpleUrlPreview(
-                                url: text,
-                                bgColor: Colors.white,
-                                titleLines: 1,
-                                descriptionLines: 2,
-                                imageLoaderColor: Colors.white,
-                                titleStyle:
-                                    Theme.of(context).textTheme.headline6,
-                                descriptionStyle:
-                                    Theme.of(context).textTheme.bodyText1,
-                                siteNameStyle:
-                                    Theme.of(context).textTheme.caption,
-                              );
+                              if (UniversalPlatform.isWeb) {
+                                return InkWell(
+                                  hoverColor: Colors.transparent,
+                                  child: Text(
+                                    text,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodyText1
+                                        ?.copyWith(color: Colors.blue),
+                                  ),
+                                  onTap: () {
+                                    launch(text, forceWebView: true);
+                                  },
+                                );
+                              }
+                              return FlutterLinkPreview(
+                                  url: text,
+                                  titleStyle:
+                                      Theme.of(context).textTheme.headline6,
+                                  bodyStyle:
+                                      Theme.of(context).textTheme.bodyText1,
+                                  builder: (info) {
+                                    switch (info.runtimeType) {
+                                      case WebInfo:
+                                        return AnyLinkPreview(
+                                          link: text,
+                                          displayDirection:
+                                              UIDirection.UIDirectionVertical,
+                                          showMultimedia: true,
+                                          bodyMaxLines: 3,
+                                          bodyTextOverflow:
+                                              TextOverflow.ellipsis,
+                                          titleStyle: Theme.of(context)
+                                              .textTheme
+                                              .headline6,
+                                          bodyStyle: Theme.of(context)
+                                              .textTheme
+                                              .bodyText1,
+                                          errorBody: 'cannot preview this link',
+                                          errorTitle: 'error',
+                                          errorWidget: InkWell(
+                                            hoverColor: Colors.transparent,
+                                            child: Text(
+                                              text,
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .bodyText1
+                                                  ?.copyWith(
+                                                      color: Colors.blue),
+                                            ),
+                                            onTap: () {
+                                              launch(text, forceWebView: true);
+                                            },
+                                          ),
+                                          errorImage: "https://google.com/",
+                                          cache: Duration(days: 7),
+                                        );
+                                      case WebImageInfo:
+                                        return SizedBox(
+                                          child: Card(
+                                            shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(
+                                                        20.0)),
+                                            clipBehavior: Clip.antiAlias,
+                                            child: CachedNetworkImage(
+                                              imageUrl:
+                                                  (info as WebImageInfo).image,
+                                              fit: BoxFit.contain,
+                                            ),
+                                          ),
+                                        );
+                                      case WebVideoInfo:
+                                        return InkWell(
+                                          hoverColor: Colors.transparent,
+                                          child: Text(
+                                            text,
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodyText1
+                                                ?.copyWith(color: Colors.blue),
+                                          ),
+                                          onTap: () {
+                                            launch(text, forceWebView: true);
+                                          },
+                                        );
+                                      default:
+                                        return const SizedBox();
+                                    }
+                                  });
+
+                              // return SimpleUrlPreview(
+                              //   url: text,
+                              //   bgColor: Colors.white,
+                              //   titleLines: 1,
+                              //   descriptionLines: 2,
+                              //   imageLoaderColor: Colors.white,
+                              //   titleStyle:
+                              //       Theme.of(context).textTheme.headline6,
+                              //   descriptionStyle:
+                              //       Theme.of(context).textTheme.bodyText1,
+                              //   siteNameStyle:
+                              //       Theme.of(context).textTheme.caption,
+                              // );
                             },
                             onTap: (url) {
                               print(url);
